f6bccd14d7af2cbfbdf7b39a6fc70bc9c257bb6a
Pranav-tv <40848975+Pranav-tv@users.noreply.github.com>
2018-08-08 17:33:12 +0530

    Add files via upload

---
+++ harvard_prob.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 16:15:55 2018
+
+@author: pranav
+"""
+
+import numpy as np
+
+def simulate_prizedoor(nsim):
+    
+    return np.array(np.random.randint(10,size = nsim))
+
+
+def simulate_guess(nsim):
+
+    return np.array(np.random.randint(10,size = nsim))
+
+def goat_door(prizedoors,guesses):
+    choices = set((0,1,2,3,4,5,6,7,8,9))
+    numbers = []
+    for x,y in zip(prizedoors, guesses):
+        numbers.append((choices - set((x,y))).pop())
+    return(np.array(numbers))
+        
+      
+def switch_guess(guesses, goatdoors):
+    choices = set((0,1,2,3,4,5,6,7,8,9))
+    numbers = []
+    for x,y in zip(guesses, goatdoors):
+        numbers.append((choices - set((x,y))).pop())
+    return(np.array(numbers))     
+
+def win_percentage(guesses, prizedoors):
+    count = 0
+    for x,y in zip(guesses, prizedoors):
+        if x == y:
+            count+=1
+    return(count/len(guesses)*100)
+
+
+nsim = int(input("Enter the number of simulations: "))
+
+prizedoors = simulate_prizedoor(nsim)
+
+guesses = simulate_guess(nsim)
+
+goatdoors = goat_door(prizedoors, guesses)
+
+switchedchoice = switch_guess(guesses, goatdoors)
+
+winning_with_original_choice = win_percentage(guesses,prizedoors) 
+
+winning_with_switched_choice = win_percentage(switchedchoice,prizedoors) 
+
+print("\nWinning % with original choice = {}".format(winning_with_original_choice))
+
+print("\nWinning % with switched choice = {}".format(winning_with_switched_choice))
+
+
+
+
+
+
+
+
+
